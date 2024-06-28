0f35ff770b80aade8064ce69ae7044b2cc197a1a
NB2612 <154438389+NB2612@users.noreply.github.com>
2024-06-28 23:34:54 +0700

    Add files via upload

---
+++ module-info.java
@@ -0,0 +1,16 @@
+module com.nb2506.image_editor {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.swing;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+
+    requires opencv;
+
+    opens com.nb2506.image_editor to javafx.fxml;
+    exports com.nb2506.image_editor;
+    exports com.nb2506.image_editor.controllers;
+    opens com.nb2506.image_editor.controllers to javafx.fxml;
+}
